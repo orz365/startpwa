6c7c2c1adc107baf3445d8401df6c8b648806b81
liuwei <orz365@163.com>
2018-08-02 15:39:25 +0800

    添加gitignore文件

--- workerservice/sw.js
+++ workerservice/sw.js
@@ -1,31 +1,31 @@
 var cacheName = 'workserservice'
 var contentToCache = [
-  '/startpwa/workerservice/app.js',
-  '/startpwa/workerservice/index.js',
-  '/startpwa/workerservice/dati.png'
+    '/startpwa/workerservice/app.js',
+    '/startpwa/workerservice/index.js',
+    '/startpwa/workerservice/dati.png'
 ]
 
 self.addEventListener('install', function (e) {
-  console.log(e)
-  e.waitUntil(
-    caches.open(cacheName).then(function (cache) {
-      console.log('[Service Worker] Caching all: app shell and content')
-      return cache.addAll(contentToCache)
-    })
-  )
+    console.log(e)
+    e.waitUntil(
+        caches.open(cacheName).then(function (cache) {
+            console.log('[Service Worker] Caching all: app shell and content')
+            return cache.addAll(contentToCache)
+        })
+    )
 })
 
 self.addEventListener('fetch', function (e) {
-  e.respondWith(
-    caches.match(e.request).then(function (r) {
-      console.log('[Service Worker] Fetching resource: ' + e.request.url)
-      return r || fetch(e.request).then(function (response) {
-        return caches.open(cacheName).then(function (cache) {
-          console.log('[Service Worker] Caching new resource: ' + e.request.url)
-          cache.put(e.request, response.clone())
-          return response
+    e.respondWith(
+        caches.match(e.request).then(function (r) {
+            console.log('[Service Worker] Fetching resource: ' + e.request.url)
+            return r || fetch(e.request).then(function (response) {
+                return caches.open(cacheName).then(function (cache) {
+                    console.log('[Service Worker] Caching new resource: ' + e.request.url)
+                    cache.put(e.request, response.clone())
+                    return response
+                })
+            })
         })
-      })
-    })
-  )
+    )
 })
